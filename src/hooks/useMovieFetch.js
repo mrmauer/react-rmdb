5be015da74e5e3a0fc88a3f7b516e6a3d45623d9
Matthew Mauer <mrmauer112@gmail.com>
2023-09-19 18:06:56 -0400

    completed react app

---
+++ src/hooks/useMovieFetch.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import API from "../API";
+import { isPersistedState } from "../helpers";
+
+export const useMovieFetch = (movieId) => {
+  const [state, setState] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        setLoading(true);
+        setError(false);
+
+        const movie = await API.fetchMovie(movieId);
+        const credits = await API.fetchCredits(movieId);
+
+        // get directors only
+        const directors = credits.crew.filter(
+          (member) => member.job === "Director"
+        );
+
+        setState({
+          ...movie,
+          actors: credits.cast,
+          directors,
+        });
+
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+      }
+    };
+
+    const sessionState = isPersistedState(movieId);
+
+    if (sessionState) {
+      console.log(`Grabbing movie ${movieId} from session storage.`);
+      setState(sessionState);
+      setLoading(false);
+      return;
+    }
+
+    console.log(`Fetching movie ${movieId} from API.`);
+    fetchMovie();
+  }, [movieId]);
+
+  useEffect(() => {
+    sessionStorage.setItem(movieId, JSON.stringify(state));
+  }, [movieId, state]);
+
+  return { state, loading, error };
+};
